4a73f55ed9ce6ff1e2d35c36870f11d281c83725
Sava Minić <savam@nordeus.eu>
2016-08-27 13:22:53 +0200

    Added selection of stars

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -3,5 +3,26 @@ using System.Collections;
 
 public class Star : MonoBehaviour
 {
+
+	public bool IsSelected { get; private set; }
+
+	private Renderer myRenderer;
+
+	void Awake()
+	{
+		myRenderer = GetComponent<Renderer>();
+	}
+
+	public void Select()
+	{
+		IsSelected = true;
+		myRenderer.material.color = Color.red;
+	}
+
+	public void Deselect()
+	{
+		IsSelected = false;
+		myRenderer.material.color = Color.white;
+	}
 	
 }
--- Assets/Scripts/StarManager.cs
+++ Assets/Scripts/StarManager.cs
@@ -1,8 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
+using Random = UnityEngine.Random;
 
 public class StarManager : MonoBehaviour
 {
+
+	#region Properties
+
+	public static StarManager Instance { get; private set; }
+
+	public enum GameState
+	{
+		Default = 0,
+		Dragging,
+	}
+
+	public GameState State { get; private set; }
+
+	#endregion
 
 	#region Public fields
 
@@ -14,35 +30,90 @@ public class StarManager : MonoBehaviour
 
 	#endregion
 
+	#region Private fields
+
 	private List<Star> allStars = new List<Star>();
+
+	private List<Star> selectedStars = new List<Star>();
+	private Star lastSelectedStar;
 
 	private GameObject starPrefab;
 
+	#endregion
+
 	void Awake()
 	{
+		Instance = this;
 		starPrefab = Resources.Load("Prefabs/Star") as GameObject;
 	}
 
 	void Start()
 	{
+		State = GameState.Default;
 		GenerateStars(10);
 	}
 
 	void Update()
 	{
-		if (Input.GetMouseButtonDown(0))
+		if (Input.GetMouseButtonDown(0) && State == GameState.Default)
 		{
-			RaycastHit hit;
-			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (Physics.Raycast(ray, out hit, float.MaxValue, starLayer))
+			OnStarHit(star =>
 			{
-				var star = hit.collider.gameObject.GetComponent<Star>();
-				if (star != null)
+				Debug.Log("HIT STAR " + star.gameObject.transform.position);
+				State = GameState.Dragging;
+				lastSelectedStar = star;
+				star.Select();
+				selectedStars.Add(star);
+			});
+		}
+		else if (Input.GetMouseButtonUp(0) && State == GameState.Dragging)
+		{
+			Debug.Log("Released!");
+			State = GameState.Default;
+			for (int i = 0; i < selectedStars.Count; i++)
+			{
+				selectedStars[i].Deselect();
+			}
+			selectedStars.Clear();
+		}
+		else if (Input.GetMouseButton(0) && State == GameState.Dragging)
+		{
+			OnStarHit(star =>
+			{
+				if (star != lastSelectedStar || lastSelectedStar == null)
 				{
-					Debug.Log("HIT STAR");
+					Debug.Log("Toggle STAR " + star.gameObject.transform.position);
+					lastSelectedStar = star;
+					if (!star.IsSelected)
+					{
+						star.Select();
+						selectedStars.Add(star);
+					}
+					else
+					{
+						star.Deselect();
+						selectedStars.Remove(star);
+					}
 				}
+			}, () => {
+				lastSelectedStar = null;
+			});
+		}
+	}
+
+	private void OnStarHit(Action<Star> callback, Action nothingCallback = null)
+	{
+		RaycastHit hit;
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit, float.MaxValue, starLayer))
+		{
+			var star = hit.collider.gameObject.GetComponent<Star>();
+			if (star != null)
+			{
+				callback(star);
 			}
 		}
+		else nothingCallback();
 	}
 
 	public void GenerateStars(int count = 5)
