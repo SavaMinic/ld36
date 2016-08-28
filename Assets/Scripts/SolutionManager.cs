736df691423f79917e612c708b6b5019b5ef22b7
Sava Minić <savam@nordeus.eu>
2016-08-28 17:42:34 +0200

    Refactored generating solutions a little, try to mitigate interesting, added type of branching

--- Assets/Scripts/SolutionManager.cs
+++ Assets/Scripts/SolutionManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System;
+using Random = UnityEngine.Random;
 
 public class SolutionManager : MonoBehaviour
 {
@@ -35,25 +36,81 @@ public class SolutionManager : MonoBehaviour
 
 	#region Public methods
 
-	public void GenerateNewSolution(int index = 0, int connectionsCount = 5)
+	public void GenerateNewSolution(int index = 0)
 	{
-		Debug.Log("Generating solution! " + connectionsCount);
+		int straightLimit = Random.Range(2,3);
+		int branchOut = Random.Range(0, 3);
+		Debug.Log("Generating solution! " + straightLimit + " branch " + branchOut);
 		Solutions[index].Clear();
 
-		var starWeights = new Dictionary<Star, int>();
-		StarManager.Instance.AllStars.ForEach(s => starWeights.Add(s, 3));
-
+		// try to make big line (up to limit)
+		var freeStars = new List<Star>(StarManager.Instance.AllStars);
 		Star from = null;
-		for (int i = 0; i < connectionsCount; i++)
+		for (int i = 0; i < straightLimit; i++)
 		{
-			var star = StarManager.Instance.AllStars.PickRandomFromWeightedList(s => starWeights[s]);
-			starWeights[star] -= 1;
-			if (from != null)
+			Star star = GetAvailableStar(Solutions[index], from);
+			if (star != null)
 			{
-				Solutions[index].Add(new SolutionConnection(from, star));
+				// TODO: check this interesctions check, seems that not working always
+				if (from != null && !IntersectsWithExisting(Solutions[index], from, star))
+				{
+					Solutions[index].Add(new SolutionConnection(from, star));
+					freeStars.Remove(from);
+				}
+				from = star;
 			}
-			from = star;
 		}
+		// remove also the last one
+		if (from != null)
+			freeStars.Remove(from);
+		
+		// do branching out
+		for (int i = 0; i < branchOut; i++)
+		{
+			BranchOut(Solutions [index], freeStars);
+		}
+	}
+
+	private void BranchOut(List<SolutionConnection> solution, List<Star> freeStars)
+	{
+		var branchOutStar = solution.GetRandom().From;
+		var freeStar = freeStars.GetRandom();
+		solution.Add(new SolutionConnection(branchOutStar, freeStar));
+		freeStars.Remove(freeStar);
+	}
+
+	private bool IntersectsWithExisting(List<SolutionConnection> solution, Star from, Star to)
+	{
+		// check all existing connections
+		for (int i = 0; i < solution.Count; i++)
+		{
+			if (Util.IsIntersecting(
+				    from.transform.position,
+				    to.transform.position,
+				    solution [i].From.transform.position,
+				    solution [i].To.transform.position
+			    ))
+			{
+				return false;
+			}
+		}
+		return false;
+	}
+
+	private Star GetAvailableStar(List<SolutionConnection> solution, Star previousStar)
+	{
+		// limit it to some value
+		int limit = StarManager.Instance.AllStars.Count;
+		for(int i = 0; i < limit; i++)
+		{
+			var star = StarManager.Instance.AllStars.GetRandom();
+			// there is no connection already like this
+			if (!solution.Exists(s => (s.To == star && s.From == previousStar) || (s.To == previousStar && s.From == star)))
+			{
+				return star;
+			}
+		}
+		return null;
 	}
 
 	// just for test
