695153ae576a88b1b006065711215369cdaf33da
Sava Minić <savam@nordeus.eu>
2016-08-28 13:35:47 +0200

    Added GoKit, implemented base for the scrolls

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class Scroll : MonoBehaviour
+{
+
+	public Vector3 endPosition;
+
+	private Vector3 startPosition;
+
+	private GoTween moveAnimation = null;
+
+	void Awake()
+	{
+		startPosition = transform.position;
+	}
+
+	void OnMouseEnter() {
+		if (moveAnimation != null)
+			moveAnimation.complete();
+		moveAnimation = Go.to(transform, .4f, new GoTweenConfig().position(endPosition).setEaseType(GoEaseType.BackInOut));
+	}
+
+	void OnMouseExit() {
+		if (moveAnimation != null)
+			moveAnimation.complete();
+		moveAnimation = Go.to(transform, .4f, new GoTweenConfig().position(startPosition).setEaseType(GoEaseType.BackInOut));
+	}
+}
