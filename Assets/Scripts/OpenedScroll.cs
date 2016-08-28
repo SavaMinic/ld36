b8f51d67fd1e00ad17249871dc687d20d4daa959
Sava Minić <savam@nordeus.eu>
2016-08-28 14:06:50 +0200

    Added opened scroll

---
+++ Assets/Scripts/OpenedScroll.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class OpenedScroll : MonoBehaviour
+{
+
+	private Renderer myRenderer;
+
+	private Vector3 defaultScale;
+
+	private GoTween toggleAnimation;
+
+	void Awake()
+	{
+		defaultScale = transform.localScale;
+		myRenderer = GetComponent<Renderer>();
+		myRenderer.enabled = false;
+	}
+
+	public void Open()
+	{
+		if (toggleAnimation != null)
+			toggleAnimation.complete();
+		if (myRenderer.enabled)
+		{
+			AnimateClose(AnimateOpening);
+		}
+		else
+		{
+			AnimateOpening();
+		}
+	}
+
+	public void AnimateClose(Action callbackOnClosed = null)
+	{
+		var endScale = new Vector3(0f, defaultScale.y, defaultScale.z);
+		toggleAnimation = Go.to(transform, 0.5f,new GoTweenConfig()
+			.vector3Prop("localScale", endScale).setEaseType(GoEaseType.BackInOut)
+			.onComplete(tw =>
+			{
+				myRenderer.enabled = false;
+				if (callbackOnClosed != null)
+					callbackOnClosed();
+			})
+		);
+	}
+
+	public void AnimateOpening()
+	{
+		transform.localScale = new Vector3(0f, defaultScale.y, defaultScale.z);
+		toggleAnimation = Go.to(transform, 0.5f, new GoTweenConfig().vector3Prop("localScale", defaultScale).setEaseType(GoEaseType.BackInOut));
+		myRenderer.enabled = true;
+	}
+
+}
--- Assets/Scripts/Scroll.cs
+++ Assets/Scripts/Scroll.cs
@@ -5,26 +5,50 @@ using System;
 public class Scroll : MonoBehaviour
 {
 
+	private static int openedScrollIndex = -1;
+
 	public Vector3 endPosition;
+	public int scrollIndex;
 
 	private Vector3 startPosition;
 
 	private GoTween moveAnimation = null;
 
+	private OpenedScroll openedScroll;
+
 	void Awake()
 	{
 		startPosition = transform.position;
+		openedScroll = FindObjectOfType<OpenedScroll>();
 	}
 
-	void OnMouseEnter() {
+	void OnMouseEnter()
+	{
 		if (moveAnimation != null)
 			moveAnimation.complete();
 		moveAnimation = Go.to(transform, .4f, new GoTweenConfig().position(endPosition).setEaseType(GoEaseType.BackInOut));
 	}
 
-	void OnMouseExit() {
+	void OnMouseExit()
+	{
 		if (moveAnimation != null)
 			moveAnimation.complete();
 		moveAnimation = Go.to(transform, .4f, new GoTweenConfig().position(startPosition).setEaseType(GoEaseType.BackInOut));
+	}
+
+	void OnMouseUp()
+	{
+		if (openedScrollIndex != scrollIndex)
+		{
+			// open scroll
+			openedScrollIndex = scrollIndex;
+			openedScroll.Open();
+		}
+		else
+		{
+			// close
+			openedScrollIndex = -1;
+			openedScroll.AnimateClose();
+		}
 	}
 }
